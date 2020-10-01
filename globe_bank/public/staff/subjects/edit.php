5636c2b923dd9e059561b097241c527336dd2b25
Ericcentrik <enaiman1@gmail.com>
2020-10-01 17:41:39 -0400

    created update functionality for subjects

--- globe_bank/public/staff/subjects/edit.php
+++ globe_bank/public/staff/subjects/edit.php
@@ -7,24 +7,26 @@ if(!isset($_GET['id'])) {
   redirect_to(url_for('/staff/subjects/index.php'));
 }
 $id = $_GET['id'];
-$menu_name = '';
-$position = '';
-$visible = '';
 
 if(is_post_request()) {
 
   // Handle form values sent by new.php
 
-  $menu_name = $_POST['menu_name'] ?? '';
-  $position = $_POST['position'] ?? '';
-  $visible = $_POST['visible'] ?? '';
+  $subject = [];
+  $subject['id'] = $id;
+  $subject['menu_name'] = $_POST['menu_name'] ?? '';
+  $subject['position'] = $_POST['position'] ?? '';
+  $subject['visible'] = $_POST['visible'] ?? '';
 
-  echo "Form parameters<br />";
-  echo "Menu name: " . $menu_name . "<br />";
-  echo "Position: " . $position . "<br />";
-  echo "Visible: " . $visible . "<br />";
+  $result = update_subject($subject);
+  redirect_to(url_for('/staff/subjects/show.php?id=' . $id));
+
+} else {
+
+  $subject = find_subject_by_id($id);
 
 }
+
 ?>
 
 <?=$page_title = 'Edit Subject'; ?>
@@ -40,13 +42,13 @@ if(is_post_request()) {
     <form action="<?= url_for('/staff/subjects/edit.php?=' . h(u($id))); ?>" method="post">
       <dl>
         <dt>Menu Name</dt>
-        <dd><input type="text" name="menu_name" value="<?= $menu_name; ?>" /></dd>
+        <dd><input type="text" name="menu_name" value="<?= h($subject['menu_name']); ?>" /></dd>
       </dl>
       <dl>
         <dt>Position</dt>
         <dd>
           <select name="position">
-            <option value="1">1</option>
+            <option value="1" <?php if($subject['position'] == "1") {echo "selected"; } ?>>1</option>
           </select>
         </dd>
       </dl>
@@ -54,7 +56,7 @@ if(is_post_request()) {
         <dt>Visible</dt>
         <dd>
           <input type="hidden" name="visible" value="0" />
-          <input type="checkbox" name="visible" value="1" />
+          <input type="checkbox" name="visible" value="1" <?php if($subject['visible'] == "1") { echo "selected"; } ?>  />
         </dd>
       </dl>
       <div id="operations">
