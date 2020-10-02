c5bd62e1eb435c102574a30c8c168ed4a6176b9b
Ericcentrik <enaiman1@gmail.com>
2020-10-02 13:10:34 -0400

    created validation and santization

--- globe_bank/private/database.php
+++ globe_bank/private/database.php
@@ -2,17 +2,23 @@
 
 require_once('db_credentials.php');
 
+// connects databas
 function db_connect() {
     $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
     confirm_db_connect();
     return $connection;
   }
-
+// disconnect db
   function db_disconnect($connection) {
     if(isset($connection)) {
       mysqli_close($connection);
     }
   }
+
+// santizes data to prevent Sql injections
+function db_escape($connection, $string){
+  return mysqli_real_escape_string($connection, $string);
+}
 
   // if database is not connected, it will throw an error
   function confirm_db_connect(){
--- globe_bank/public/staff/subjects/edit.php
+++ globe_bank/public/staff/subjects/edit.php
@@ -8,6 +8,7 @@ if(!isset($_GET['id'])) {
 }
 $id = $_GET['id'];
 
+
 if(is_post_request()) {
 
   // Handle form values sent by new.php
@@ -18,19 +19,22 @@ if(is_post_request()) {
   $subject['position'] = $_POST['position'] ?? '';
   $subject['visible'] = $_POST['visible'] ?? '';
 
+
+
   $result = update_subject($subject);
-  redirect_to(url_for('/staff/subjects/show.php?id=' . $id));
-
+  if($result === true) {
+    redirect_to(url_for('/staff/subjects/show.php?id=' . $id));
+  } else {
+    $errors = $result;
+    // var_dump($errors);
+  }
+ 
 } else {
-
   $subject = find_subject_by_id($id);
-
-  $subject_set = find_all_subjects();
-  $subject_count = mysqli_num_rows($subject_set);
-  mysqli_free_result($subject_set);
-
 }
-
+$subject_set = find_all_subjects();
+$subject_count = mysqli_num_rows($subject_set);
+mysqli_free_result($subject_set);
 ?>
 
 <?php $page_title = 'Edit Subject'; ?>
@@ -43,6 +47,7 @@ if(is_post_request()) {
   <div class="subject edit">
     <h1>Edit Subject</h1>
 
+    <?= display_errors($errors);?>
     <form action="<?= url_for('/staff/subjects/edit.php?id=' . h(u($id))); ?>" method="post">
       <dl>
         <dt>Menu Name</dt>
